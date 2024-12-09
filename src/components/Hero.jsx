110d543b9dc89e5d6a4b97a38feab5ea7dad2189
mahamudul hasan seber <sebermh@gmail.com>
2024-12-09 21:09:13 +0600

    Hero section ok

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,51 @@
 import endgame from "../assets/endgame.jpg"
 import spyder from "../assets/spyderman.jpg"
 import venom from "../assets/venom.jpg"
+import superman from "../assets/superman.jpeg";
 const Hero = () => {
     return (
-        <div className="carousel carousel-center rounded-box lg:pl-60 pt-10">
-            <div className="carousel-item">
-                <img src={endgame} alt="photo" />
+        <div>
+            <div className="carousel h-[600px]">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img
+                        src={endgame}
+                        className="w-full" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img
+                        src={spyder}
+                        className="w-full" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img
+                        src={venom}
+                        className="w-full" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img
+                        src={superman}
+                        className="w-full" />
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
             </div>
-            <div className="carousel-item">
-                <img
-                    src={spyder}
-                    alt="photo" />
-            </div>
-            <div className="carousel-item">
-                <img
-                    src={venom}
-                    alt="photo" />
-            </div>
-
-
         </div>
+
+
     );
 };
 
