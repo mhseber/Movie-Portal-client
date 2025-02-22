96cc0080756c97ed551c42f8e31db747f39c6a68
mahamudul hasan seber <sebermh@gmail.com>
2025-02-22 19:15:23 +0600

    AddMovies Section

--- src/Pages/Community.jsx
+++ src/Pages/Community.jsx
@@ -16,45 +16,63 @@ const Community = () => {
             </div>
             {/* Community & Forums */}
             <div>
-                <h1 className="text-3xl font-bold pt-4 pl-4">Latest Discussions</h1>
+                <h1 data-aos="slide-right" className="text-3xl font-bold pt-4 pl-4">Latest Discussions</h1>
             </div>
             {/* card */}
             <section>
                 {/* 1 */}
                 <div className="p-4">
-                    <div className="card bg-sky-400 text-primary-content p-2">
+                    <div
+                        data-aos="zoom-in"
+                        data-aos-delay="300"
+                        className="card bg-sky-400 text-primary-content p-2">
                         <div className="card-body">
-                            <h2 className="card-title text-3xl font-bold text-blue-500">Favorite Movies of 2023?</h2>
-                            <p className="text-gray-500 font-medium">Leo doo | 2 hours ago</p>
+                            <h2 data-aos="slide-right" className="card-title text-3xl font-bold text-blue-500">Favorite Movies of 2023?</h2>
+                            <p data-aos="slide-right" className="text-gray-500 font-medium">Leo doo | 2 hours ago</p>
                             <p>What are your favorite movies released this year? Let's discuss the top picks!</p>
                             <div className="card-actions ">
-                                <button className="btn bg-blue-900 text-white border-none">Join Discussions</button>
+                                <button
+                                    data-aos="fade-up"
+                                    data-aos-delay="600"
+                                    className="btn bg-blue-900 text-white border-none">Join Discussions</button>
                             </div>
                         </div>
                     </div>
                 </div>
                 {/* 2 */}
                 <div className="p-4">
-                    <div className="card bg-sky-400 text-primary-content p-2">
+                    <div
+                        data-aos="zoom-in"
+                        data-aos-delay="300"
+                        className="card bg-sky-400 text-primary-content p-2">
                         <div className="card-body">
-                            <h2 className="card-title text-3xl font-bold text-blue-500">Best Movie Soundtracks?</h2>
-                            <p className="text-gray-500 font-medium">Kord a | 5 hours ago</p>
+                            <h2 data-aos="slide-right" className="card-title text-3xl font-bold text-blue-500">Best Movie Soundtracks?</h2>
+                            <p data-aos="slide-right" className="text-gray-500 font-medium">Kord a | 5 hours ago</p>
                             <p>Which movies have soundtracks that you can't stop listening to?</p>
                             <div className="card-actions ">
-                                <button className="btn bg-blue-900 text-white border-none">Join Discussions</button>
+                                <button
+                                    data-aos="fade-up"
+                                    data-aos-delay="600"
+                                    className="btn bg-blue-900 text-white border-none">Join Discussions</button>
                             </div>
                         </div>
                     </div>
                 </div>
                 {/* 3 */}
                 <div className="p-4">
-                    <div className="card bg-sky-400 text-primary-content p-2">
+                    <div
+                        data-aos="zoom-in"
+                        data-aos-delay="300"
+                        className="card bg-sky-400 text-primary-content p-2">
                         <div className="card-body">
-                            <h2 className="card-title text-3xl font-bold text-blue-500">Underrated Directors</h2>
-                            <p className="text-gray-500 font-medium">By Alex Green | 1 day ago</p>
+                            <h2 data-aos="slide-right" className="card-title text-3xl font-bold text-blue-500">Underrated Directors</h2>
+                            <p data-aos="slide-right" className="text-gray-500 font-medium">By Alex Green | 1 day ago</p>
                             <p>Who are some directors that don't get the recognition they deserve?</p>
                             <div className="card-actions ">
-                                <button className="btn bg-blue-900 text-white border-none">Join Discussions</button>
+                                <button
+                                    data-aos="fade-up"
+                                    data-aos-delay="600"
+                                    className="btn bg-blue-900 text-white border-none">Join Discussions</button>
                             </div>
                         </div>
                     </div>
@@ -63,7 +81,7 @@ const Community = () => {
             {/* Start a Discussion */}
             <section>
                 <div>
-                    <h1 className="text-3xl font-bold pt-4 pl-4">Start a Discussion</h1>
+                    <h1 data-aos="slide-right" className="text-3xl font-bold pt-4 pl-4">Start a Discussion</h1>
                 </div>
                 {/* form */}
                 <div className=" bg-sky-200 shadow-2xl">
--- src/Pages/MyFavorites.jsx
+++ src/Pages/MyFavorites.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
+import Aos from 'aos';
+import React, { useEffect } from 'react';
+import { GiSelfLove } from 'react-icons/gi';
 
 const MyFavorites = () => {
+    useEffect(() => {
+        Aos.init({ duration: 1000, once: true });
+    }, []);
     return (
         <div>
-            myFavorites
+            {/* Header */}
+            <div className=' bg-gradient-to-r from-indigo-500 via-purple-500 to-black text-white p-6'>
+                <div className='flex justify-between'>
+                    <div className='p-6'>
+                        <h1 className='text-6xl font-bold'>Your Favorites List</h1>
+                        <p className='text-gray-200 text-xl pt-4'>All your favorite items in one place. Organize, view, and manage with ease.</p>
+                    </div>
+                    <div className='text-8xl pt-8 pr-40'>
+                        <GiSelfLove />
+                    </div>
+                </div>
+            </div>
+            {/* body */}
+            <div className="hero bg-slate-400 min-h-screen">
+                <div className="hero-content text-center">
+                    <div className="max-w-md">
+                        <h1
+                            data-aos="fade-up"
+                            data-aos-delay="600"
+                            className="text-5xl font-bold">No Favorites Found</h1>
+                        <p
+                            data-aos="slide-right"
+                            className="py-6 font-semibold">
+                            You have not added any movies to your favorites list yet.
+                        </p>
+
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
