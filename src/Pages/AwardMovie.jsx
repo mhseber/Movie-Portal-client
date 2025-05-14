d8a0eb7f5a04edb95f3d749ac367475f65b89f62
mahamudul hasan seber <sebermh@gmail.com>
2025-05-15 03:04:17 +0600

    Projects Responsive

--- src/Pages/AwardMovie.jsx
+++ src/Pages/AwardMovie.jsx
@@ -1,3 +1,140 @@
+// import Aos from "aos";
+// import endgame from "../assets/endgame.jpg";
+// import spyder from "../assets/spyderman.jpg";
+// import venom from "../assets/venom.jpg";
+// import "aos/dist/aos.css";
+// import { useEffect } from "react";
+// import { Link } from "react-router-dom";
+
+// const AwardMovie = () => {
+//   useEffect(() => {
+//     Aos.init({ duration: 1000, once: true });
+//   }, []);
+
+//   return (
+//     <div className=" bg-gradient-to-r from-blue-900 to-blue-950 w-[500px]border border-8 pt-8">
+//       <div className="text-center">
+//         <h1
+//           data-aos="fade-up"
+//           data-aos-delay="600"
+//           className="text-4xl font-semibold text-white"
+//         >
+//           Award-Winning Movies
+//         </h1>
+//         <p
+//           data-aos="slide-left"
+//           className="pt-4 text-xs font-semibold text-gray-400"
+//         >
+//           Discover the movies that have captivated audiences and critics alike.
+//         </p>
+//       </div>
+//       <div className="flex justify-center gap-3 p-5">
+//         {/* card  1*/}
+//         <div className="shadow-xl card card-compact bg-base-100 w-96">
+//           <figure>
+//             <img
+//               data-aos="zoom-in"
+//               data-aos-delay="200"
+//               className="w-full h-[276px]"
+//               src={endgame}
+//               alt="img"
+//             />
+//           </figure>
+//           <div className="card-body">
+//             <h2 className="text-2xl font-extrabold text-black card-title">
+//               EndGame
+//             </h2>
+//             <p data-aos="slide-right" className="text-gray-500">
+//               Oscar Award for Best Picture
+//             </p>
+//             <p data-aos="slide-right" className="text-black">
+//               <span className="font-extrabold">Year: </span> 2020
+//             </p>
+//             <div className="justify-end card-actions">
+//               <Link
+//                 to="/home/learnMore"
+//                 data-aos="fade-up"
+//                 data-aos-delay="200"
+//                 className="text-white bg-black btn hover:bg-gray-900"
+//               >
+//                 Learn More
+//               </Link>
+//             </div>
+//           </div>
+//         </div>
+//         {/* card  2*/}
+//         <div className="shadow-xl card card-compact bg-base-100 w-96">
+//           <figure>
+//             <img
+//               data-aos="zoom-in"
+//               data-aos-delay="200"
+//               className="w-full h-[276px]"
+//               src={venom}
+//               alt="img"
+//             />
+//           </figure>
+//           <div className="card-body">
+//             <h2 className="text-2xl font-extrabold text-black card-title">
+//               Venom 2
+//             </h2>
+//             <p data-aos="slide-right" className="text-gray-500">
+//               Oscar Award for Best Picture
+//             </p>
+//             <p data-aos="slide-right" className="text-black">
+//               <span className="font-extrabold">Year: </span>2021
+//             </p>
+//             <div className="justify-end card-actions">
+//               <Link
+//                 to="/home/learnMore"
+//                 data-aos="fade-up"
+//                 data-aos-delay="200"
+//                 className="text-white bg-black btn hover:bg-gray-900"
+//               >
+//                 Learn More
+//               </Link>
+//             </div>
+//           </div>
+//         </div>
+//         {/* card 3*/}
+//         <div className="shadow-xl card card-compact bg-base-100 w-96">
+//           <figure>
+//             <img
+//               data-aos="zoom-in"
+//               data-aos-delay="200"
+//               className="w-full h-[276px]"
+//               src={spyder}
+//               alt="img"
+//             />
+//           </figure>
+//           <div className="card-body">
+//             <h2 className="text-2xl font-extrabold text-black card-title">
+//               SpyderMan 2
+//             </h2>
+//             <p data-aos="slide-right" className="text-gray-500">
+//               Oscar Award for Best Picture
+//             </p>
+//             <p data-aos="slide-right" className="text-black">
+//               <span className="font-extrabold">Year: </span>2023
+//             </p>
+//             <div className="justify-end card-actions">
+//               <Link
+//                 to="/home/learnMore"
+//                 data-aos="fade-up"
+//                 data-aos-delay="200"
+//                 className="text-white bg-black btn hover:bg-gray-900"
+//               >
+//                 Learn More
+//               </Link>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default AwardMovie;
+
 import Aos from "aos";
 import endgame from "../assets/endgame.jpg";
 import spyder from "../assets/spyderman.jpg";
@@ -12,7 +149,7 @@ const AwardMovie = () => {
   }, []);
 
   return (
-    <div className=" bg-gradient-to-r from-blue-900 to-blue-950 w-[500px]border border-8 pt-8">
+    <div className="w-full pt-8 border-8 bg-gradient-to-r from-blue-900 to-blue-950">
       <div className="text-center">
         <h1
           data-aos="fade-up"
@@ -28,16 +165,18 @@ const AwardMovie = () => {
           Discover the movies that have captivated audiences and critics alike.
         </p>
       </div>
-      <div className="flex justify-center gap-3 p-5">
-        {/* card  1*/}
-        <div className="shadow-xl card card-compact bg-base-100 w-96">
+
+      {/* Flex container to make the cards responsive */}
+      <div className="flex flex-wrap justify-center gap-4 p-5">
+        {/* Card 1 */}
+        <div className="w-full shadow-xl card card-compact bg-base-100 sm:w-80 md:w-96">
           <figure>
             <img
               data-aos="zoom-in"
               data-aos-delay="200"
               className="w-full h-[276px]"
               src={endgame}
-              alt="img"
+              alt="Endgame"
             />
           </figure>
           <div className="card-body">
@@ -62,15 +201,16 @@ const AwardMovie = () => {
             </div>
           </div>
         </div>
-        {/* card  2*/}
-        <div className="shadow-xl card card-compact bg-base-100 w-96">
+
+        {/* Card 2 */}
+        <div className="w-full shadow-xl card card-compact bg-base-100 sm:w-80 md:w-96">
           <figure>
             <img
               data-aos="zoom-in"
               data-aos-delay="200"
               className="w-full h-[276px]"
               src={venom}
-              alt="img"
+              alt="Venom 2"
             />
           </figure>
           <div className="card-body">
@@ -95,15 +235,16 @@ const AwardMovie = () => {
             </div>
           </div>
         </div>
-        {/* card 3*/}
-        <div className="shadow-xl card card-compact bg-base-100 w-96">
+
+        {/* Card 3 */}
+        <div className="w-full shadow-xl card card-compact bg-base-100 sm:w-80 md:w-96">
           <figure>
             <img
               data-aos="zoom-in"
               data-aos-delay="200"
               className="w-full h-[276px]"
               src={spyder}
-              alt="img"
+              alt="SpyderMan 2"
             />
           </figure>
           <div className="card-body">
--- src/Pages/Community.jsx
+++ src/Pages/Community.jsx
@@ -1,109 +1,374 @@
+// import { useEffect, useRef, useState } from "react";
+// import Aos from "aos";
+// import Swal from "sweetalert2";
+
+// const Community = ({ user }) => {
+//   const [discussions, setDiscussions] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [title, setTitle] = useState("");
+//   const [content, setContent] = useState("");
+
+//   const formRef = useRef(null);
+
+//   useEffect(() => {
+//     Aos.init({ duration: 1000, once: true });
+
+//     // Simulate page load delay with spinner
+//     setTimeout(() => {
+//       // Preloaded discussions
+//       setDiscussions([
+//         {
+//           title: "Favorite Movies of 2023?",
+//           user: "Leo doo",
+//           time: "2 hours ago",
+//           content:
+//             "What are your favorite movies released this year? Let's discuss the top picks!",
+//         },
+//         {
+//           title: "Best Movie Soundtracks?",
+//           user: "Kord a",
+//           time: "5 hours ago",
+//           content:
+//             "Which movies have soundtracks that you can't stop listening to?",
+//         },
+//         {
+//           title: "Underrated Directors",
+//           user: "Alex Green",
+//           time: "1 day ago",
+//           content:
+//             "Who are some directors that don't get the recognition they deserve?",
+//         },
+//       ]);
+//       setLoading(false);
+//     }, 2000);
+//   }, []);
+
+//   const handleSubmit = () => {
+//     if (!title || !content) {
+//       Swal.fire({
+//         icon: "warning",
+//         title: "Please fill out both fields",
+//       });
+//       return;
+//     }
+
+//     const newDiscussion = {
+//       title,
+//       user: user?.name || "You",
+//       time: "just now",
+//       content,
+//     };
+
+//     setDiscussions([newDiscussion, ...discussions]);
+//     setTitle("");
+//     setContent("");
+
+//     Swal.fire({
+//       position: "top-end",
+//       icon: "success",
+//       title: "Your discussion has been posted",
+//       showConfirmButton: false,
+//       timer: 1500,
+//     });
+//   };
+
+//   const handleJoinDiscussionClick = () => {
+//     formRef.current?.scrollIntoView({ behavior: "smooth" });
+//   };
+
+//   return (
+//     <div>
+//       {/* Header */}
+//       <div className="p-6 text-center text-white bg-black">
+//         <h1 className="text-4xl font-bold">Community & Forums</h1>
+//         <p className="pt-4 text-gray-400">
+//           Connect with fellow movie enthusiasts and share your thoughts!
+//         </p>
+//       </div>
+
+//       {/* Latest Discussions */}
+//       <div>
+//         <h1 data-aos="slide-right" className="pt-4 pl-4 text-3xl font-bold">
+//           Latest Discussions
+//         </h1>
+//       </div>
+
+//       {/* Spinner */}
+//       {loading ? (
+//         <div className="flex justify-center py-8">
+//           <span className="loading loading-bars loading-xl"></span>
+//         </div>
+//       ) : (
+//         <section>
+//           {discussions.map((discussion, index) => (
+//             <div key={index} className="p-4">
+//               <div
+//                 data-aos="zoom-in"
+//                 data-aos-delay="300"
+//                 className="card bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-primary-content p-2"
+//               >
+//                 <div className="card-body">
+//                   <h2
+//                     data-aos="slide-right"
+//                     className="text-3xl font-bold text-blue-500 card-title"
+//                   >
+//                     {discussion.title}
+//                   </h2>
+//                   <p
+//                     data-aos="slide-right"
+//                     className="font-medium text-gray-200"
+//                   >
+//                     {discussion.user} | {discussion.time}
+//                   </p>
+//                   <p>{discussion.content}</p>
+//                   <div className="card-actions">
+//                     <button
+//                       onClick={handleJoinDiscussionClick}
+//                       data-aos="fade-up"
+//                       data-aos-delay="600"
+//                       className="text-white bg-blue-900 border-none btn"
+//                     >
+//                       Join Discussions
+//                     </button>
+//                   </div>
+//                 </div>
+//               </div>
+//             </div>
+//           ))}
+//         </section>
+//       )}
+
+//       {/* Start a Discussion */}
+//       <section ref={formRef}>
+//         <div>
+//           <h1 data-aos="slide-right" className="pt-4 pl-4 text-3xl font-bold">
+//             Start a Discussion
+//           </h1>
+//         </div>
+
+//         <div className="shadow-2xl bg-sky-200">
+//           <div className="p-8 mt-3">
+//             <h1 className="p-2 font-bold text-black">Title</h1>
+//             <input
+//               type="text"
+//               placeholder="Enter the discussion title"
+//               className="w-full input input-bordered input-info"
+//               value={title}
+//               onChange={(e) => setTitle(e.target.value)}
+//             />
+
+//             <h1 className="p-2 font-bold text-black">Content</h1>
+//             <textarea
+//               className="w-full textarea textarea-info"
+//               placeholder="Write your thoughts here..."
+//               value={content}
+//               onChange={(e) => setContent(e.target.value)}
+//             ></textarea>
+
+//             <div className="mt-6">
+//               <button
+//                 onClick={handleSubmit}
+//                 className="font-bold btn btn-primary"
+//               >
+//                 Post Discussion
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+//       </section>
+//     </div>
+//   );
+// };
+
+// export default Community;
+
+import { useEffect, useRef, useState } from "react";
 import Aos from "aos";
-import { useEffect } from "react";
+import Swal from "sweetalert2";
+import "aos/dist/aos.css";
 
+const Community = ({ user }) => {
+  const [discussions, setDiscussions] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [loading, setLoading] = useState(true);
+  const formRef = useRef(null);
 
-const Community = () => {
-    useEffect(() => {
-        Aos.init({ duration: 1000, once: true });
-    }, []);
+  useEffect(() => {
+    Aos.init({ duration: 1000, once: true });
 
-    return (
-        <div>
-            {/* Header */}
-            <div className='text-center bg-black text-white p-6'>
-                <h1 className='text-4xl font-bold'>Community & Forums</h1>
-                <p className='text-gray-400 pt-4'>Connect with fellow movie enthusiasts and share your thoughts!</p>
-            </div>
-            {/* Community & Forums */}
-            <div>
-                <h1 data-aos="slide-right" className="text-3xl font-bold pt-4 pl-4">Latest Discussions</h1>
-            </div>
-            {/* card */}
-            <section>
-                {/* 1 */}
-                <div className="p-4">
-                    <div
-                        data-aos="zoom-in"
-                        data-aos-delay="300"
-                        className="card bg-sky-400 text-primary-content p-2">
-                        <div className="card-body">
-                            <h2 data-aos="slide-right" className="card-title text-3xl font-bold text-blue-500">Favorite Movies of 2023?</h2>
-                            <p data-aos="slide-right" className="text-gray-500 font-medium">Leo doo | 2 hours ago</p>
-                            <p>What are your favorite movies released this year? Let's discuss the top picks!</p>
-                            <div className="card-actions ">
-                                <button
-                                    data-aos="fade-up"
-                                    data-aos-delay="600"
-                                    className="btn bg-blue-900 text-white border-none">Join Discussions</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* 2 */}
-                <div className="p-4">
-                    <div
-                        data-aos="zoom-in"
-                        data-aos-delay="300"
-                        className="card bg-sky-400 text-primary-content p-2">
-                        <div className="card-body">
-                            <h2 data-aos="slide-right" className="card-title text-3xl font-bold text-blue-500">Best Movie Soundtracks?</h2>
-                            <p data-aos="slide-right" className="text-gray-500 font-medium">Kord a | 5 hours ago</p>
-                            <p>Which movies have soundtracks that you can't stop listening to?</p>
-                            <div className="card-actions ">
-                                <button
-                                    data-aos="fade-up"
-                                    data-aos-delay="600"
-                                    className="btn bg-blue-900 text-white border-none">Join Discussions</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* 3 */}
-                <div className="p-4">
-                    <div
-                        data-aos="zoom-in"
-                        data-aos-delay="300"
-                        className="card bg-sky-400 text-primary-content p-2">
-                        <div className="card-body">
-                            <h2 data-aos="slide-right" className="card-title text-3xl font-bold text-blue-500">Underrated Directors</h2>
-                            <p data-aos="slide-right" className="text-gray-500 font-medium">By Alex Green | 1 day ago</p>
-                            <p>Who are some directors that don't get the recognition they deserve?</p>
-                            <div className="card-actions ">
-                                <button
-                                    data-aos="fade-up"
-                                    data-aos-delay="600"
-                                    className="btn bg-blue-900 text-white border-none">Join Discussions</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            {/* Start a Discussion */}
-            <section>
-                <div>
-                    <h1 data-aos="slide-right" className="text-3xl font-bold pt-4 pl-4">Start a Discussion</h1>
-                </div>
-                {/* form */}
-                <div className=" bg-sky-200 shadow-2xl">
-                    <div className="p-8 mt-3">
-                        <h1 className="p-2 text-black font-bold">Title</h1>
-                        <input
-                            type="text"
-                            placeholder="enter the discussion title"
-                            className="input input-bordered input-info w-full " />
-                        <h1 className="p-2 text-black font-bold">Content</h1>
-                        <textarea
-                            className="textarea textarea-info w-full"
-                            placeholder="Right your thoughts here..."></textarea>
+    setTimeout(() => {
+      const stored = localStorage.getItem("movieDiscussions");
+      if (stored) {
+        setDiscussions(JSON.parse(stored));
+      } else {
+        const initial = [
+          {
+            title: "Favorite Movies of 2023?",
+            user: "Leo doo",
+            time: "2 hours ago",
+            content:
+              "What are your favorite movies released this year? Let's discuss the top picks!",
+          },
+          {
+            title: "Best Movie Soundtracks?",
+            user: "Kord a",
+            time: "5 hours ago",
+            content:
+              "Which movies have soundtracks that you can't stop listening to?",
+          },
+          {
+            title: "Underrated Directors",
+            user: "Alex Green",
+            time: "1 day ago",
+            content:
+              "Who are some directors that don't get the recognition they deserve?",
+          },
+        ];
+        setDiscussions(initial);
+        localStorage.setItem("movieDiscussions", JSON.stringify(initial));
+      }
+      setLoading(false);
+    }, 1000);
+  }, []);
 
-                        <div className=" mt-6">
-                            <button className="btn btn-primary  font-bold">Post Discussion</button>
-                        </div>
-                    </div>
-                </div>
-            </section>
+  const handleSubmit = () => {
+    if (!title || !content) {
+      Swal.fire({
+        icon: "warning",
+        title: "Please fill out both fields",
+      });
+      return;
+    }
+
+    const newPost = {
+      title,
+      user: user?.name || "You",
+      time: "just now",
+      content,
+    };
+
+    const updated = [newPost, ...discussions];
+    setDiscussions(updated);
+    localStorage.setItem("movieDiscussions", JSON.stringify(updated));
+
+    setTitle("");
+    setContent("");
+
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Your discussion has been posted",
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  };
+
+  const scrollToForm = () => {
+    formRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  return (
+    <div>
+      {/* Header */}
+      <div className="p-6 text-center text-white bg-black">
+        <h1 className="text-4xl font-bold">Community & Forums</h1>
+        <p className="pt-4 text-gray-400">
+          Connect with fellow movie enthusiasts and share your thoughts!
+        </p>
+      </div>
+
+      {/* Discussions */}
+      <div>
+        <h1 data-aos="slide-right" className="pt-4 pl-4 text-3xl font-bold">
+          Latest Discussions
+        </h1>
+      </div>
+
+      {loading ? (
+        <div className="flex justify-center py-10">
+          <span className="loading loading-bars loading-xl"></span>
         </div>
-    );
+      ) : (
+        <section>
+          {discussions.map((post, index) => (
+            <div className="p-4" key={index}>
+              <div
+                data-aos="zoom-in"
+                data-aos-delay="300"
+                className="card bg-gradient-to-r from-indigo-700 from-10% via-indigo-600 via-30% to-sky-500 to-90% text-primary-content p-2"
+              >
+                <div className="card-body">
+                  <h2
+                    data-aos="slide-right"
+                    className="text-3xl font-bold text-blue-200 card-title"
+                  >
+                    {post.title}
+                  </h2>
+                  <p
+                    data-aos="slide-right"
+                    className="font-medium text-gray-200"
+                  >
+                    {post.user} | {post.time}
+                  </p>
+                  <p className="text-white">{post.content}</p>
+                  <div className="card-actions">
+                    <button
+                      onClick={scrollToForm}
+                      data-aos="fade-up"
+                      data-aos-delay="600"
+                      className="text-white bg-black border-none hover:bg-gray-900 btn"
+                    >
+                      Join Discussions
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </section>
+      )}
+
+      {/* Start a Discussion */}
+      <section ref={formRef}>
+        <div>
+          <h1 data-aos="slide-right" className="pt-4 pl-4 text-3xl font-bold">
+            Start a Discussion
+          </h1>
+        </div>
+
+        <div className="shadow-2xl bg-slate-200">
+          <div className="p-8 mt-3">
+            <h1 className="p-2 font-bold text-black">Title</h1>
+            <input
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="Enter the discussion title"
+              className="w-full input input-bordered input-info"
+            />
+
+            <h1 className="p-2 font-bold text-black">Content</h1>
+            <textarea
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              className="w-full textarea textarea-info"
+              placeholder="Write your thoughts here..."
+            ></textarea>
+
+            <div className="mt-6">
+              <button
+                onClick={handleSubmit}
+                className="font-bold text-white bg-black btn hover:bg-gray-900"
+              >
+                Post Discussion
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Community;
